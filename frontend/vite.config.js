1330f0128b93f7c56dc925335ab5c0498703d4f8
sayan-coder-mondal <sayanmondal132@gmail.com>
2025-05-18 03:18:52 +0530

    18th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://mern-deployment-test.vercel.app/',
+      '/api': 'https://mern-deployment-test.vercel.app',
     },
   },
 })
